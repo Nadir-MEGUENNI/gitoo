821aa5380e67c20c4b1562ebbed878c291d456b1
ThabetRebhi <thabet.rebhi@gmail.com>
2021-03-14 14:33:44 +0100

    adding zinja

--- gito/ViewController.swift
+++ gito/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("zinja")
         // Do any additional setup after loading the view.
     }
-
+//jjjjjjjj
+    
 
 }
 
